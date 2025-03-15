a54f5b0dbfb6451dc5697c0fae470c03c31c0782
Rory Sullivan <codingrory@gmail.com>
2025-03-15 08:54:58 -0700

    Adds three JS with basic scene

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+import * as three from "three";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +21,27 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+// Create our scene
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add a cube
+const geometry = new three.BoxGeometry(1, 1, 1);
+const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new three.Mesh(geometry, material);
+scene.add(cube);
+
+function animationLoop() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.02;
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animationLoop);
